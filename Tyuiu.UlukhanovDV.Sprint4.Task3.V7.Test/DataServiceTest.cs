29c6229bfbbd8137ab52a9e29596e6536d751168
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-11-06 16:25:23 +0500

    Task3

---
+++ Tyuiu.UlukhanovDV.Sprint4.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Tyuiu.UlukhanovDV.Sprint4.Task3.V7.Lib;
+namespace Tyuiu.UlukhanovDV.Sprint4.Task3.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mass = new int[5, 5] { { 9, 6, 9, 3, 7, }, { 3, 3, 3, 8, 2, }, { 2, 1, 3, 5, 2, }, { 6, 2, 3, 2, 5, }, { 4, 5, 6, 9, 5, } };
+            int res = ds.Calculate(mass);
+            int wait = 73;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
