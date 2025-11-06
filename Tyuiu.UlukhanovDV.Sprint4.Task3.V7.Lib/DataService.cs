4afb8af85574e02dbbd08c790f5f742b2ed9bddf
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-11-06 16:31:43 +0500

    Task3

---
+++ Tyuiu.UlukhanovDV.Sprint4.Task3.V7.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.UlukhanovDV.Sprint4.Task3.V7.Lib
+{
+    public class DataService : ISprint4Task3V7
+    {
+        public int Calculate(int[,] array)
+        {
+            int r = array.GetUpperBound(0) + 1;
+            int co = array.Length / r;
+            int s = 0;
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < co; j++)
+                {
+                    if (array[i, j] % 2 != 0)
+                    {
+                        s++;
+                    }
+                }
+
+            }
+            return s;
+        }
+    }
+}
